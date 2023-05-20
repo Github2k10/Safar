092294b2ff80e732dc958e8b4f120fe8a810ea4b
Ankit Kumar <65845325+Github2k10@users.noreply.github.com>
2023-05-21 02:06:41 +0530

    update

--- FrontEnd/js/signup.js
+++ FrontEnd/js/signup.js
@@ -1,17 +1,47 @@
+let email = "abc@gmail.com";
+let password = "abc123";
+
 const loginText = document.querySelector(".title-text .login");
 const loginForm = document.querySelector("form.login");
 const loginBtn = document.querySelector("label.login");
 const signupBtn = document.querySelector("label.signup");
 const signupLink = document.querySelector("form .signup-link a");
+
+
 signupBtn.onclick = (()=>{
   loginForm.style.marginLeft = "-50%";
   loginText.style.marginLeft = "-50%";
+
+  document.querySelector("#loginEmail").value = "";
+  document.querySelector("#loginPassword").value = "";
 });
+
 loginBtn.onclick = (()=>{
   loginForm.style.marginLeft = "0%";
   loginText.style.marginLeft = "0%";
 });
+
 signupLink.onclick = (()=>{
   signupBtn.click();
   return false;
 });
+
+function login(){
+  let mail = document.querySelector("#loginEmail");
+  let pass = document.querySelector("#loginPassword");
+
+  if(mail.value == email && pass.value == password){
+    document.querySelector("#success").classList.add("open-success");
+  } else {
+    document.querySelector("#error").classList.add("open-error");
+  }
+}
+
+function closeSuccess(){
+  document.querySelector("#success").classList.remove("open-success");
+}
+
+function closeError(){
+  document.querySelector("#error").classList.remove("open-error");
+}
+
